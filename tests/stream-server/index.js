26f213fdd3a669ec0f712f7222465250506543cf
Bespredel <bespredel.owner@mail.ru>
2025-11-14 08:31:39 +0300

    refactor: modernize test infrastructure
    - Remove deprecated docker-compose version field
    - Convert stream server to ES modules syntax
    - Update autotest UI theme from light to dark graphite

--- tests/stream-server/index.js
+++ tests/stream-server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { WebSocketServer } = require('ws');
-const http = require('http');
+import express from 'express';
+import cors from 'cors';
+import { WebSocketServer } from 'ws';
+import http from 'http';
 
 const app = express();
 const server = http.createServer(app);
